caf751edf6649d90d5c4908a62fcbb2869e6343b
Eles <1494136313@qq.com>
2024-03-21 00:19:14 +0800

    first commit

---
+++ src/Square.tsx
@@ -0,0 +1,14 @@
+interface SquareProps {
+  value: string;
+  onSquareClick: () => void;
+}
+
+const Square = ({ value, onSquareClick }: SquareProps) => {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Square;
